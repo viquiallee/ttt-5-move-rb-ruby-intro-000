b40cff5ba548838e93a194607ae48aea24120efe
Victoria Alee <victoriaallee@gmail.com>
2020-11-19 23:00:50 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,10 +10,17 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def
 update_array_at_with(array, index, value)
-board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+array[index] = value
 end
 
- update_array_at_with(board, "O", "X")
+update_array_at_with(board, "O", "X")
+ 
+board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+input_to_index
+'5'to_i
+end
+
+
  
 
 
