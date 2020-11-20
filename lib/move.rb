ccf5c36bc2cef123e96acc5f693bcbd71ed5181a
Victoria Alee <victoriaallee@gmail.com>
2020-11-20 00:32:35 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index("1", "2", "3", "4", "5", "6", "7", "8", "9")
-  "5".to_i
+def input_to_index("1".to_i, "2".to_i, "3".to_i, "4".to_i, "5".to_i, "6".to_i, "7".to_i, "8".to_i, "9".to_i)
   5 - 1 
 end 
 
