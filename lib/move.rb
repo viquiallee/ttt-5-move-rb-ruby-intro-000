c3e11ef56ad64eb30dfa93985b92dd1d7dbc19d5
Victoria Alee <victoriaallee@gmail.com>
2020-11-20 01:06:05 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,15 +6,15 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index("1".to_i, "2".to_i, "3".to_i, "4".to_i, "5".to_i, "6".to_i, "7".to_i, "8".to_i, "9".to_i)
-  5 - 1 
-end 
+# def input_to_index("1".to_i, "2".to_i, "3".to_i, "4".to_i, "5".to_i, "6".to_i, "7".to_i, "8".to_i, "9".to_i)
+#   5 - 1 
+# end 
 
-def move
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  def
-  update_array_at_with(array, index, value)
-    array[index] = value
-end
+# def move
+#   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#   def
+#   update_array_at_with(array, index, value)
+#     array[index] = value
+# end
 
-update_array_at_with(board, "0", "X")
+# update_array_at_with(board, "0", "X")
